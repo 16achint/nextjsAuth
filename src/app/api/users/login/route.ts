8692de7ce5cb7db3239972d36ffa3d04ab19c96d
achint <achintj96244@gmail.com>
2025-08-08 15:18:02 +0530

    Add user authentication pages and functionality for login, signup, and email verification

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -41,7 +41,6 @@ export async function POST(request: NextRequest) {
       { message: "login successFully", token: token, success: true },
       { status: 200 }
     );
-
     response.cookies.set("token", token, { httpOnly: true });
 
     return response;
